b25577c74d7377500184e83ad244ede3e2b13f36
Bruno Wotzke <brunoricardowotzke@gmail.com>
2023-03-07 12:51:09 -0300

    .

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -3,38 +3,55 @@ import React, { useState, useEffect } from "react";
 export default function App(): JSX.Element {
 
   const getELements = () => {
-    let outCam = document.querySelector("#camera");
-    let stopCam = document.querySelector("#stop");
+      let outCam = document.querySelector("#camera");
+      let btCamera = document.querySelector("#btCamera");
 
-    return {
-      outCam: outCam,
-      stopCam: stopCam
-    };
+      return {
+        outCam: outCam,
+        btCamera: btCamera
+      }
+  }
+
+  let saveData = {
+    cameraTrack: [{}, false]
   }
 
   async function enableAudio() {
     await navigator.mediaDevices.getUserMedia({"audio": true});
   }
 
+  function stopCam(){
+    let tx = saveData.cameraTrack[0];
 
-  async function enableCamera() {
-    await navigator.mediaDevices.getUserMedia({"video": true})
-    .then(e => {
-      let track = e.getVideoTracks()[0];
+    tx.stop();
 
-      (getELements().outCam as HTMLVideoElement).srcObject = e;
-
-      (getELements().stopCam as HTMLElement).onclick = () => {
-        track.stop()
-      }
-    })
+    saveData.cameraTrack[1] = false;
   }
+
+    function enableCamera() {
+      navigator.mediaDevices.getUserMedia({"video": true})
+      .then(e => {
+        let track = e.getVideoTracks()[0];
+
+        saveData.cameraTrack[0] = track;
+        saveData.cameraTrack[1] = true;
+
+        (getELements().outCam as HTMLVideoElement).srcObject = e;
+      })
+    }
 
 
   async function enableVideo() {
     const outScreen = document.querySelector("#screen");
     let videoC = await navigator.mediaDevices.getDisplayMedia();
     (outScreen as HTMLVideoElement).srcObject = videoC;
+  }
+
+  function btCam(){
+    let tm = saveData.cameraTrack[1];
+
+    tm == false ? enableCamera() : stopCam();
+    
   }
 
   return(
@@ -44,8 +61,7 @@ export default function App(): JSX.Element {
       <br/>
       <div>
         <button onClick={() => enableAudio()}>🎙️</button>
-        <button onClick={() => enableCamera()}>📷</button>
-        <button id="stop">-📷-</button>
+        <button id="btCamera" data-tm="enable" onClick={() => btCam()}>📷</button>
         <button onClick={() => enableVideo()}>🖥️</button>
       </div>
       <br/>
